072087de8dc37ab270f87b16fbb75d1f62a16cd4
Mukul29-Dev <dev.mukul.3y.29@gmail.com>
2023-12-18 23:10:11 +0530

    Header-added

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ function App() {
   return (
     <div className="App">
       <react-fragment>
-        <div className="bg-main">
-          <>
+        <>
           <Header />
-          </>
-        </div>
+        </>
       </react-fragment>
     </div>
   );
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,35 +1,35 @@
 import React from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import './header.css'
 
 function Header() {
     return (
-        <div className="d-flex text-center">
-            <div className="mt-5">
-                <h1>Kanban</h1>
-            </div>
-            <div>
-                <Container className="mt-5">
-                    <Row>
-                        <Col sm={4}>
-                        <Form className="d-flex">
+        <Container className="mt-3 header-bg">
+            <div className="row d-flex text-center mx-auto">
+                <div className="d-inline-block col-md-4">
+                    <h1>KANBAN</h1>
+                </div>
+                <div className="col-md-4">
+                    <Form inline w-100>
+                        <Row className=" justify-content-between">
+                        <Col xs="auto col-md-9 text-end">
                             <Form.Control
-                            type="search"
+                            type="text"
                             placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
+                            className=" mr-sm-2"
                             />
-                            <Button>
-                            Search
-                            </Button>
-                        </Form>
                         </Col>
-                    </Row>
-                </Container>
+                        <Col xs="auto col-md-3">
+                            <Button type="submit">Submit</Button>
+                        </Col>
+                        </Row>
+                    </Form>
+                </div>
+                <div className="col-md-4" >
+                    <h4> Login </h4>
+                </div>
             </div>
-            <div className="mt-5">
-                Account login
-            </div>
-        </div>
+        </Container>
     );
 }
 export default Header;
