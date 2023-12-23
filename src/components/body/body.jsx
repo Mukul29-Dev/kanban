56b499095ec8d10952323fb177bc210026efb01a
Mukul29-Dev <dev.mukul.3y.29@gmail.com>
2023-12-23 15:52:13 +0530

    Pc-New_changes

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -23,49 +23,16 @@ export default function body() {
                 </Card>
             </Col>
             <Col xs="col text-center Kan_col mt-5">
-                <h3 className="mt-2 column_head" > In-Progress </h3>
-                <Card className="mx-auto mt-3 card_shadow" style={{ width: '23rem' }}>
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                            </Card.Text>
-                            <Card.Link href="#">Card Link</Card.Link>
-                            <Card.Link href="#">Another Link</Card.Link>
-                        </Card.Body>
-                </Card>
+                <h3 className="mt-2 column_head mx-auto" > In-Progress </h3>
+                
             </Col>
             <Col xs="col text-center Kan_col mt-5">
                 <h3 className="mt-2 column_head" > In-review </h3>
-                <Card className="mx-auto mt-3 card_shadow" style={{ width: '23rem' }}>
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                            </Card.Text>
-                            <Card.Link href="#">Card Link</Card.Link>
-                            <Card.Link href="#">Another Link</Card.Link>
-                        </Card.Body>
-                </Card>
+                
             </Col>
             <Col xs="col text-center Kan_col mt-5">
                 <h3 className="mt-2 column_head"> Completed </h3>
-                <Card className="mx-auto mt-3 card_shadow" style={{ width: '23rem' }}>
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                            </Card.Text>
-                            <Card.Link href="#">Card Link</Card.Link>
-                            <Card.Link href="#">Another Link</Card.Link>
-                        </Card.Body>
-                </Card>
+                
             </Col>
         
         </Row>
